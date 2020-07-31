557603edf8c178dee559654fd78f1fff61112c4b
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-31 20:29:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -62,7 +62,7 @@ def dennis_ritchies_language
         }
      }
      binding.pry
-     return programmer_hash[:dennis_ritchie][:languages].to_s
+     mystring = programmer_hash[:dennis_ritchie][:languages].join
 end
 
 def adding_matz
