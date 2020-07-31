69164bc885c3944bfc38641c349856e77bf1ba13
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-31 20:24:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -59,7 +59,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     return programmer_hash[:dennis_ritchie][:languages].to_s
+     return (programmer_hash[:dennis_ritchie][:languages]).to_s
 end
 
 def adding_matz
