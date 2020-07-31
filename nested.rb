df531d03b43535012ee2418530c8c1bfc1a6c0f8
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-31 20:45:09 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -93,6 +93,8 @@ def adding_matz
           :languages => ["LISP", "C"]
      }
 
+     return programmer_hash
+
     # programmer_hash {} =
     # :yukihiro_matsumoto => {
     #   :known_for => "Ruby",
